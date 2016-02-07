a9ba4d63e075ce38480764c58c8e8536e0a04d9f
Raphael Kottakal <raphael@webmanifestation.com>
2016-02-08 01:15:16 +0530

    Status checks, loading image array with callback and start qNa render

--- js/data.js
+++ js/data.js
@@ -1,4 +1,65 @@
 var dataObject = {
 	backgroundColor: '#3AA9DB',
-	backgroundImage: 'img/background.png'
+	backgroundImage: 'img/background.png',
+	test: [
+		{
+			question: {
+				text: 'This is the first question. Click on an option to proceed.',
+				style: {
+					background: 'http://www.fluitecnik.com/default/templates/front/001/imagenes/Img_Banner_Sun.jpg',
+				}
+			},
+			options: [
+				{
+					text: 'This is the first option',
+					style: {
+						background: 'http://www.gettyimages.in/gi-resources/images/CreativeImages/Hero-527920799.jpg',
+					}
+				},
+				{
+					text: 'This is the first option',
+					style: {
+						background: 'http://stories.gettyimages.com/wp-content/uploads/2015/08/GettyImages-557187411-11.jpg',
+					}
+				},
+				{
+					text: 'This is the first option',
+					style: {
+						background: 'http://im.rediff.com/news/2015/dec/24tpoty20.jpg',
+					}
+				},
+
+			]
+		},
+
+		{
+			question: {
+				text: 'This is the first question. Click on an option to proceed.',
+				style: {
+					background: 'http://images.panda.org/assets/images/pages/welcome/orangutan_1600x1000_279157.jpg',
+				}
+			},
+			options: [
+				{
+					text: 'This is the first option',
+					style: {
+						background: 'http://www.freeitblog.com/wp-content/uploads/images-10.jpg',
+					}
+				},
+				{
+					text: 'This is the first option',
+					style: {
+						background: 'http://wwwimages.adobe.com/content/dam/acom/en/products/creativecloud/stock/cc2015/images/st-selling-images-434x280.jpg',
+					}
+				},
+				{
+					text: 'This is the first option',
+					style: {
+						background: 'https://www.dreamhost.com/blog/wp-content/uploads/2015/10/DHC_blog-image-01-300x300.jpg',
+					}
+				},
+
+			]
+		}
+	]
 }
--- js/main.js
+++ js/main.js
@@ -27,14 +27,67 @@
 
 					        	// console.log($.inArray(false, loadedArray));
 
-					        	if ($.inArray(false, loadedArray) == -1 ) {
-					        		$overlay.fadeOut(1000);
-					        	};
+					        	// if ($.inArray(false, loadedArray) == -1 ) {
+					        	// 	$overlay.fadeOut(1000);
+					        	// };
 
 					            callback();
 					        }
-						})
+						});
 	};
+
+	function loadImages(array, callback) {
+
+		var loadedImages = {};
+
+		$.each(array, function(key, src) {
+
+			loadedImages[key] = false;
+
+			var $img = $('<img/>').attr('src', src)
+							.load(function() {
+								if (!this.complete || typeof this.naturalWidth == "undefined" || this.naturalWidth == 0) {
+						            return 'broken image!';
+						        } else {
+						        	var loadedArray = [];
+						        	loadedImages[key] = true;
+						        	$.each(loadedImages, function(key, loaded) {
+						        		// console.log(key, loaded);
+						        		loadedArray.push(loaded);
+						        	});
+
+
+						        	// console.log($.inArray(false, loadedArray));
+
+						        	if ($.inArray(false, loadedArray) == -1 ) {
+						        		// $overlay.fadeOut(1000);
+						        		console.log('all images loaded');
+						        		callback();
+						        	};
+						        }
+							});
+
+		});
+
+
+	};
+
+	// function loadAllImages(test) {
+	// 	console.log('loading all images', test);
+
+	// 	// need to know the structer of test object
+	// 	$.each(test, function(i, val) {
+	// 		// question
+	// 		loadImgResource( i + '-question', val.question.style.background, function() { console.log('loaded:', i + '-question') });
+
+	// 		//options
+	// 		$.each(val.options, function(x, val) {
+	// 			// console.log(x,val);
+	// 			loadImgResource( i + '-'+ x + '-option', val.style.background, function() { console.log('loaded:',i + '-'+ x + '-option') });
+	// 		});
+	// 	});
+
+	// };
 
 	function getWindowHeight() {
 		var windowHeight = $(window).outerHeight();
@@ -68,6 +121,7 @@
 
 	// main plugin
 	$.fn.eqTest = function(data) {
+
 		var $this = this;
 		// console.log($this);
 
@@ -82,11 +136,84 @@
 		loadImgResource('background', backgroundImage, function() {
 			console.log('background loaded!');
 			$this.css({'background-image': 'url(' + backgroundImage + ')'});
+			// loadAllImages(data.test);
+			render.trigger();
 		});
-		loadImgResource('test', 'http://www.planwallpaper.com/static/images/Winter-Tiger-Wild-Cat-Images.jpg', function() {
-			console.log('test loaded');
-			// $this.css({'background-image': 'url(' + backgroundImage + ')'});
-		});
+		// loadImgResource('test', 'http://www.planwallpaper.com/static/images/Winter-Tiger-Wild-Cat-Images.jpg', function() {
+		// 	console.log('test loaded');
+		// 	// $this.css({'background-image': 'url(' + backgroundImage + ')'});
+		// });
+	
+		//
+		var render = {
+
+			meta: {
+				status: 'loading',
+
+			},
+
+			checkStatus: function() {
+				return this.meta.status;
+			},
+
+			setStatus: function(status) {
+				this.meta.status = status;
+			},
+
+			getObject: function(key) {
+
+				var key = parseInt(key);
+
+				return data.test[key];
+			},
+
+			trigger: function() {
+				var status = this.checkStatus();
+
+				console.log('status:',status);
+
+				if (status == 'loading') {
+
+					this.setStatus('0');
+
+					this.qNaRender();
+				};
+			},
+
+			qNaRender: function() {
+				console.log("let's start rendering");
+
+				var status = this.checkStatus(),
+					imagesArray = new Array();
+
+				console.log('status:',status);
+
+				// render status qNa object
+				// get corresponding object
+				var data = this.getObject(status);
+
+				console.log('current obj', data);
+
+				// load images
+
+				imagesArray.push(data.question.style.background);
+
+				$.each(data.options, function(x, option) {
+					imagesArray.push(option.style.background);
+				});
+
+				loadImages(imagesArray, function() {
+					console.log('All images loaded from array:', imagesArray);
+
+					$overlay.fadeOut(1000);
+
+					//render qNa obejct
+
+					// $this.append($('<img/>', { src: data.question.style.background }));
+				})
+
+			}
+		}
 	}
 
 	// var backgroundUrl = controller.getBackgroundUrl();
